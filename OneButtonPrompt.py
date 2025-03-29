23dbc33fd2fb5e49f8792da358e54dda435bec2f
billwuhao <billwuhao.2018@gmail.com>
2025-03-30 04:07:03 +0800

    Refactoring project

--- OneButtonPrompt.py
+++ OneButtonPrompt.py
@@ -1,147 +1,365 @@
 import random
 import os
+import numpy as np
+import json
+import re
+from PIL import Image, ImageSequence, ImageOps
+import torch
+import requests
+from io import BytesIO
 
 
 node_dir = os.path.dirname(os.path.abspath(__file__))
-jsonfile_path = os.path.join(node_dir, "/txtfiles/")
+prompts_file_path = os.path.join(node_dir, "files", "prompts")
+imgs_file_path = os.path.join(node_dir, "files", "images")
+imgs_prompts_path = os.path.join(node_dir, "files", "prompts-images")
 
-def process_txt_file(txtfile: str):
-    file_path = os.path.join(node_dir, "./txtfiles/", txtfile)
 
-    if os.path.exists(file_path):
-        with open(file_path, 'r', encoding='utf-8') as file:
-            lines = file.readlines()
-            processed_lines = [line.split("//")[0].strip() for line in lines if line.strip() and not line.lstrip().startswith("//")]
-            if processed_lines:
-                return processed_lines
-            else:
-                file_path = os.path.join(node_dir, "./txtfiles/", "example_" + txtfile)
-    else:
-        file_path = os.path.join(node_dir, "./txtfiles/", "example_" + txtfile)
-
-    with open(file_path, 'r', encoding='utf-8') as file:
+def get_prompts_from_txtfile(txtfile: str):
+    with open(txtfile, 'r', encoding='utf-8') as file:
         lines = file.readlines()
         processed_lines = [line.split("//")[0].strip() for line in lines if line.strip() and not line.lstrip().startswith("//")]
-        return processed_lines
-
-humans = process_txt_file("humans.txt")
-others = process_txt_file("others.txt")
-poses = process_txt_file("poses.txt")
-styles = process_txt_file("styles.txt")
-test_prompts = process_txt_file("test.txt")
-subject = ["human", "other", "dual_subject", "None"]
-
-def generate_prompt(subject: str, pose: bool, style: bool, lora_trigger_or_prefix: str, refresh: bool, test: bool, seed: int):
-
-    if refresh:
-        global humans, others, poses, styles
+        if processed_lines:
+            return processed_lines
+        else:
+            return [""]
         
-        humans = process_txt_file("humans.txt")
-        others = process_txt_file("others.txt")
-        poses = process_txt_file("poses.txt")
-        styles = process_txt_file("styles.txt")
 
-    if seed > 0:
-        random.seed(seed)
-
-    prompt_human = random.choice(humans)
-    prompt_other = random.choice(others)
-    prompt_pose = random.choice(poses)
-    prompt_style = random.choice(styles)
-
-    if subject == "human":
-        prompt_subject = prompt_human
-
-    if subject == "other":
-        prompt_subject = prompt_other
-
-    if subject == "dual_subject":
-        prompt_subject = prompt_human + ", " + prompt_other
-
-    if subject == "None":
-        prompt_subject = ""
-
-    if pose == True:
-        if prompt_subject:
-            prompt_subject = prompt_subject + ", " + prompt_pose
+def get_imageurls_from_mdfile(mdfile: str):
+    with open(mdfile, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+        processed_lines = [line.strip().strip("![]()") for line in lines if line.lstrip().startswith("![]")]
+        if processed_lines:
+            return processed_lines
         else:
-            prompt_subject = prompt_pose
+            return []
+        
 
-    if lora_trigger_or_prefix:
-        if lora_trigger_or_prefix.strip():
-            lora_trigger_or_prefix = lora_trigger_or_prefix.strip() + ", "
+def get_imageurls_prompts_from_jsonfile(jsonfile: str):
+    with open(jsonfile, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        return data
 
-    if style == True:
-        prompt = lora_trigger_or_prefix + prompt_subject + ", " + prompt_style
+
+def find_exact_word(text, word):
+    return bool(re.search(rf"\b{word}\b", text))
+
+
+def search_word_from_prompts(data: dict, word: str):
+    processed_data = {}
+    keys = list(data.keys())
+    string_values = [data[key][1] for key in keys] 
+
+    arr = np.array(string_values)
+    vectorized_find = np.vectorize(find_exact_word)
+    mask = vectorized_find(arr, word)
+    processed_data = {keys[i]: data[keys[i]] for i in range(len(keys)) if mask[i]}
+    
+    return processed_data
+
+
+def find_files_by_type(folder_path, file_extension):
+    matching_files = []
+    for filename in os.listdir(folder_path):
+        if filename.endswith(file_extension):
+            matching_files.append(filename)
+
+    return matching_files
+
+
+
+def get_image_data_from_url(url, load_time, proxies=None):
+    """
+    Checks if a URL is likely an image and returns the image data if it is.
+
+    Args:
+        url (str): The URL to check.
+
+    Returns:
+        bytes or None:
+        - Image data (bytes) if the URL is likely an image.
+        - None if the URL is not likely an image or if there was an error.
+    """
+    try:
+        response = requests.get(url, stream=True, allow_redirects=True, timeout=10, proxies=proxies)
+        response.raise_for_status()  # Raise HTTPError for bad responses (4xx or 5xx)
+
+        content_type = response.headers.get('Content-Type', '').lower()
+
+        if content_type.startswith('image/'):
+            image_content = response.content
+            import time
+            time.sleep(load_time)
+            if image_content is None:
+                return None
+            return Image.open(BytesIO(image_content))  # Return the image data as bytes
+        else:
+            return None  # Not an image content type
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching URL: {url}. Error: {e}")
+        return None  # Error during request
+    
+
+def pil2tensor(img):
+    output_images = []
+    output_masks = []
+    for i in ImageSequence.Iterator(img):
+        i = ImageOps.exif_transpose(i)
+        if i.mode == 'I':
+            i = i.point(lambda i: i * (1 / 255))
+        image = i.convert("RGB")
+        image = np.array(image).astype(np.float32) / 255.0
+        image = torch.from_numpy(image)[None,]
+        if 'A' in i.getbands():
+            mask = np.array(i.getchannel('A')).astype(np.float32) / 255.0
+            mask = 1. - torch.from_numpy(mask)
+        else:
+            mask = torch.zeros((64,64), dtype=torch.float32, device="cpu")
+        output_images.append(image)
+        output_masks.append(mask.unsqueeze(0))
+
+    if len(output_images) > 1:
+        output_image = torch.cat(output_images, dim=0)
+        output_mask = torch.cat(output_masks, dim=0)
     else:
-        prompt = lora_trigger_or_prefix + prompt_subject
-    
-    if test:
-        if not test_prompts:
-            prompt = prompt
-        else:
-            prompt = lora_trigger_or_prefix + test_prompts.pop(0)
+        output_image = output_images[0]
+        output_mask = output_masks[0]
 
-    return prompt
+    return (output_image, output_mask)
 
-class OneButtonPromptFlux:
 
-    CATEGORY = "MW-OneButtonPrompt"
-    RETURN_TYPES = ("STRING",)
-    RETURN_NAMES = ("prompt",)
-    FUNCTION = "fluxprompt"
-    
+PROMPTS = None
+IMAGES =  None
+IMAGES_PROMPTS = None
+
+
+class LoadPrompt:
+    txt_list = find_files_by_type(prompts_file_path, ".txt")
     @classmethod
     def INPUT_TYPES(cls):
-               
         return {
             "required": {
-                "refresh": ("BOOLEAN", {"default": False}),
+                "txt_1": (cls.txt_list,),
+                "txt_2": (cls.txt_list + ["None"],),
+                "txt_3": (cls.txt_list + ["None"],),
+                "txt_4": (cls.txt_list + ["None"],),
+                "txt_5": (cls.txt_list + ["None"],),
                 },
             "optional": {
-                "subject": (subject, {
-                    "default": "human", "tooltip": "'dual_subject' including both. 'None' will be no subject."
-                }),
-                "pose": ("BOOLEAN", {"default": False, "tooltip": "The pose of any subject."}),
-                "style": ("BOOLEAN", {"default": False}),
-                "lora_trigger_or_prefix": ("STRING", {
-                    "multiline": False, 
-                    "default": "", "tooltip": "Lora trigger words or custom prefix."
-                }),
-                "test": ("BOOLEAN", {"default": False}),
+                "refresh": ("BOOLEAN", {"default": False}),
                 "seed": ("INT", {"default": 0, "min": 0, "max": 0xFFFFFFFFFFFFFFFF}),
             },
         }
 
-    def fluxprompt(
+
+    CATEGORY = "🎤MW/MW-OneButtonPrompt"
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("prompt",)
+    FUNCTION = "loadprompt"
+    
+    def loadprompt(
             self, 
-            subject: str = "human", 
-            pose: bool = False, 
-            style: bool = False,
-            lora_trigger_or_prefix: str = "",
+            txt_1: str, 
+            txt_2: str, 
+            txt_3: str, 
+            txt_4: str, 
+            txt_5: str,
             refresh: bool = False,
-            test: bool = False,
-            seed: int = 0):
+            seed: int = 0
+            ):
+        
+        if seed != 0:
+            random.seed(seed)
+        
+        global PROMPTS
+        if refresh:
+            PROMPTS = None
+        
+        if PROMPTS is None:
+            PROMPTS = []
+            prompt1 = get_prompts_from_txtfile(os.path.join(prompts_file_path, txt_1))
+            PROMPTS.append(prompt1)
+            if txt_2 != "None":
+                prompt2 = get_prompts_from_txtfile(os.path.join(prompts_file_path, txt_2))
+                PROMPTS.append(prompt2)
+            if txt_3 != "None":
+                prompt3 = get_prompts_from_txtfile(os.path.join(prompts_file_path, txt_3))
+                PROMPTS.append(prompt3)
+            if txt_4 != "None":
+                prompt4 = get_prompts_from_txtfile(os.path.join(prompts_file_path, txt_4))
+                PROMPTS.append(prompt4)
+            if txt_5 != "None":
+                prompt5 = get_prompts_from_txtfile(os.path.join(prompts_file_path, txt_5))
+                PROMPTS.append(prompt5)
 
-        return (generate_prompt(subject, pose, style, lora_trigger_or_prefix, refresh, test, seed),)
+        np_data = np.array(PROMPTS, dtype=object)
+        lengths = np.array([len(sublist) for sublist in np_data])
+        random_indices = np.random.randint(0, lengths)
+        result = [sublist[index] for sublist, index in zip(np_data, random_indices)]
+
+        return ("; ".join(result),)
 
 
-from .DeepSeekRone_Qwen import DeepseekRun, QwenLLMRun, QwenVLRun
-from .LoadCivitai import LoadImageInfoFromCivitai
+class LoadImageFromURL:
+    md_list = find_files_by_type(imgs_file_path, ".md")
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "md_1": (cls.md_list,),
+                "md_2": (cls.md_list + ["None"],),
+                },
+            "optional": {
+                "load_time": ("FLOAT", {"default": 2, "min": 0.0, "max": 10, "step": 0.5}),
+                "refresh": ("BOOLEAN", {"default": False}),
+                "proxy": ("STRING", {"default": "http://127.0.0.1:None"}),
+                "seed": ("INT", {"default": 0, "min": 0, "max": 0xFFFFFFFFFFFFFFFF}),
+            },
+        }
+
+
+    CATEGORY = "🎤MW/MW-OneButtonPrompt"
+    RETURN_TYPES = ("IMAGE", "STRING",)
+    RETURN_NAMES = ("Image", "Imgurl",)
+    FUNCTION = "loadimage"
+    
+    def loadimage(
+            self, 
+            md_1: str, 
+            md_2: str, 
+            load_time: float = 2,
+            refresh: bool = False,
+            proxy: str = "http://127.0.0.1:None",
+            seed: int = 0
+            ):
+        
+        if seed != 0:
+            random.seed(seed)
+        
+        global IMAGES
+        if refresh:
+            IMAGES = None
+        
+        if IMAGES is None:
+            IMAGES = []
+            imgurl1 = get_imageurls_from_mdfile(os.path.join(imgs_file_path, md_1))
+            IMAGES.extend(imgurl1)
+            if md_2 != "None":
+                imgurl2 = get_imageurls_from_mdfile(os.path.join(imgs_file_path, md_2))
+                IMAGES.extend(imgurl2)
+                if len(IMAGES) == 0:
+                    raise ValueError("No image URL found.")
+        
+        imgurl = random.choice(IMAGES)
+        if proxy.strip() in ["http://127.0.0.1:None", ""]:
+            proxy = None
+        else:
+            proxies = {
+                "http": proxy,
+                "https": proxy,
+            }
+        img = get_image_data_from_url(imgurl, load_time, proxies=proxies)
+        if img is None:
+            raise ValueError("Failed to load image from URL. Please check the URL or proxy settings.")
+        img = img.convert("RGBA")
+        img, _ = pil2tensor(img)
+
+        return (img, imgurl)
+
+
+
+class LoadImageAndPromptFromURL:
+    json_list = find_files_by_type(imgs_prompts_path, ".json")
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "json_1": (cls.json_list,),
+                "json_2": (cls.json_list + ["None"],),
+                },
+            "optional": {
+                "load_time": ("FLOAT", {"default": 2, "min": 0.0, "max": 10, "step": 0.5}),
+                "refresh": ("BOOLEAN", {"default": False}),
+                "proxy": ("STRING", {"default": "http://127.0.0.1:None"}),
+                "on_search": ("BOOLEAN", {"default": False}),
+                "search_for": ("STRING", {"default": "cat"}),
+                "seed": ("INT", {"default": 0, "min": 0, "max": 0xFFFFFFFFFFFFFFFF}),
+            },
+        }
+
+
+    CATEGORY = "🎤MW/MW-OneButtonPrompt"
+    RETURN_TYPES = ("IMAGE", "STRING", "STRING",)
+    RETURN_NAMES = ("Image", "Prompt", "Imgurl",)
+    FUNCTION = "loadimageprompt"
+    
+    def loadimageprompt(
+            self, 
+            json_1: str, 
+            json_2: str, 
+            load_time: float = 2,
+            refresh: bool = False,
+            proxy: str = "http://127.0.0.1:None",
+            on_search: bool = False,
+            search_for: str = "cat",
+            seed: int = 0
+            ):
+        
+        if seed != 0:
+            random.seed(seed)
+        
+        global IMAGES_PROMPTS
+        if refresh:
+            IMAGES_PROMPTS = None
+        
+        if IMAGES_PROMPTS is None:
+            IMAGES_PROMPTS = {}
+            imgs_prompts1 = get_imageurls_prompts_from_jsonfile(os.path.join(imgs_prompts_path, json_1))
+            IMAGES_PROMPTS.update(imgs_prompts1)
+            if json_2 != "None":
+                imgs_prompts2 = get_imageurls_prompts_from_jsonfile(os.path.join(imgs_prompts_path, json_2))
+                IMAGES_PROMPTS.update(imgs_prompts2)
+            if len(IMAGES_PROMPTS) == 0:
+                raise ValueError("There is no content in the JSON file.")
+            
+        if on_search:
+            word = search_for.strip()
+            search_imgs_prompts = search_word_from_prompts(IMAGES_PROMPTS, word)
+            if len(search_imgs_prompts) == 0:
+                raise ValueError("No prompt found with the search word.")
+            else:
+                imgurl = random.choice(list(search_imgs_prompts.keys()))
+                prompt = search_imgs_prompts[imgurl][1]
+        else:
+            imgurl = random.choice(list(IMAGES_PROMPTS.keys()))
+            prompt = IMAGES_PROMPTS[imgurl][1]
+
+        if proxy.strip() in ["http://127.0.0.1:None", ""]:
+            proxy = None
+        else:
+            proxies = {
+                "http": proxy,
+                "https": proxy,
+            }
+        img = get_image_data_from_url(imgurl, load_time, proxies=proxies)
+        if img is None:
+            raise ValueError("Failed to load image from URL. Please check the URL or proxy settings.")
+        img = img.convert("RGBA")
+        img, _ = pil2tensor(img)
+
+        return (img, prompt, imgurl)
+    
 
 
 NODE_CLASS_MAPPINGS = {
-    "DeepseekRun": DeepseekRun,
-    "QwenLLMRun": QwenLLMRun,
-    "QwenVLRun": QwenVLRun,
-    "OneButtonPromptFlux": OneButtonPromptFlux,
-    "LoadImageInfoFromCivitai": LoadImageInfoFromCivitai
+    "LoadPrompt": LoadPrompt,
+    "LoadImageFromURL": LoadImageFromURL,
+    "LoadImageAndPromptFromURL": LoadImageAndPromptFromURL,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "DeepseekRun": "Deepseek Run",
-    "QwenLLMRun": "Qwen LLM Run",
-    "QwenVLRun": "Qwen VL Run",
-    "OneButtonPromptFlux": "One Button Prompt Flux",
-    "LoadImageInfoFromCivitai": "Load Image Info From Civitai"
+    "LoadPrompt": "Load Prompt",
+    "LoadImageFromURL": "Load Image From URL",
+    "LoadImageAndPromptFromURL": "Load Image And Prompt From URL",
 }
--- README.md
+++ README.md
@@ -1,142 +1,24 @@
 [中文](README-CN.md) | [English](README.md)
 
 ![One Button Prompt for Flux in ComfyUI](https://github.com/billwuhao/ComfyUI_OneButtonPrompt_Flux/blob/master/images/example.png)
+![](https://github.com/billwuhao/ComfyUI_OneButtonPrompt_Flux/blob/master/images/2025-02-19_20-00-01.png.png)
 
 ---
 
-# A node for one click generation of prompts (for image and video generation, etc.) in Comfyui
+# A node in comfyui for one-click assisted prompt generation (for image and video generation, etc.).
 
-## Summary
+(https://github.com/billwuhao/ComfyUI_OneButtonPrompt_Flux/blob/master/images/2025-03-30_03-22-41.png)
 
-This is a node for generating Flux prompts with one click in ComfyUI.
+## Updates
 
-## 📣 Updates
+[2025-03-30]⚒️: Completely refactored, released v1.0.0. I apologize for any inconvenience caused. If you have any suggestions or comments, please feel free to submit them in RP. Large language models are too powerful, so I decided to refactor this project. Only starting prompts are provided. Combined with large language models, they can be used for polishing, optimization, and generating various prompts (currently only suitable for image and video generation).
 
-[2025-03-01]⚒️: 
+The starting prompt folder is located in the `ComfyUI_OneButtonPrompt_Flux/files` directory, categorized as `images` (.txt files), `prompts` (.md files), and `prompts-images` (.json files). You can freely customize files within these folders, but the content format must be consistent. Custom file names must start with `nsfw` or `custom` to avoid being affected by updates.
 
-- Supports Qwen2.5 language and visual models, can generate prompts, and can describe images and videos.
-  1. Generate prompts:
-  ![](https://github.com/billwuhao/ComfyUI_OneButtonPrompt_Flux/blob/master/images/3bflux.png)
-  2. Two images are used to generate the video prompt:
-  ![](https://github.com/billwuhao/ComfyUI_OneButtonPrompt_Flux/blob/master/images/nextimage.png)
-  3. Describe video (high memory usage, please load fewer frames):
-  ![](https://github.com/billwuhao/ComfyUI_OneButtonPrompt_Flux/blob/master/images/video3b.png)
+(https://github.com/billwuhao/ComfyUI_OneButtonPrompt_Flux/blob/master/images/2025-03-30_03-52-46.png)
 
-- Now it is possible to load `onlyprompt` from JSON files on C site without the need for internet connection.
-  ![](https://github.com/billwuhao/ComfyUI_OneButtonPrompt_Flux/blob/master/images/onlyprompt.png)
+`prompts-images` supports searching prompts by specific words. For example, entering `cat` or `dog` will return prompts containing `cat` or `dog`.
 
-Download the following 3 models to `models/LLM`:
-- [Qwen2.5-VL-3B-Instruct](https://huggingface.co/Qwen/Qwen2.5-VL-3B-Instruct)
-- [Qwen2.5-3B-Instruct](https://huggingface.co/Qwen/Qwen2.5-3B-Instruct)
-- [Qwen2.5-3B-Instruct-Flux](https://huggingface.co/mrkrak3n/Qwen2.5-3B-Instruct-Flux)
+## Acknowledgments
 
-[2025-02-20]⚒️: Support [C](https://civitai.com/images) Station images and prompts.
-
-![](https://github.com/billwuhao/ComfyUI_OneButtonPrompt_Flux/blob/master/images/2025-02-23_00-40-23.png)
-
-Default use [Civitai](https://civitai.com/images). If you need to use images from other websites, please modify the file `\ComfyUI_OneButtonPrompt_Flux\txtfiles\civit_sfw.json` yourself. I will periodically update the `civit_sfw.json` file
-
-If you need nsfw images, please create a new `civit_nsfw.json` file in the `ComfyUI_OneButtonPrompt_Flux\txtfiles\` folder, which should be in the same format as the content of the `civit_sfw.json` file.
-
-Collaborate with Deepseek r1 to optimize and enhance prompts.
-
-![](https://github.com/billwuhao/ComfyUI_OneButtonPrompt_Flux/blob/master/images/2025-02-23_01-14-08.png)
-
-Reverse inference prompts.
-
-![](https://github.com/billwuhao/ComfyUI_OneButtonPrompt_Flux/blob/master/images/2025-02-23_01-37-50.png)
-
-[2025-02-19] ⚒️: Support local DeepSeek R1.
-
-![](https://github.com/billwuhao/ComfyUI_OneButtonPrompt_Flux/blob/master/images/2025-02-19_10-32-16.png)
-
-[DeepScaleR-1.5B-Preview](https://hf-mirror.com/agentica-org/DeepScaleR-1.5B-Preview) Exceeding the performance of OpenAI's O1 Preview. Manually download the model and all configuration files to `ComfyUI\models\LLM\DeepScaleR-1.5B-Preview` to use
-
--Custom theme usage:
-![](https://github.com/billwuhao/ComfyUI_OneButtonPrompt_Flux/blob/master/images/2025-02-19_19-22-49.png)
-
--Advanced automatic prompt usage:
-![](https://github.com/billwuhao/ComfyUI_OneButtonPrompt_Flux/blob/master/images/2025-02-19_19-19-26.png)
-
-This way, `{ a beautiful girl | a handsome man | a sexy woman | a naughty child | a future interstellar animal | a transportation  vehicle| a space vehicle | A fantastic building | a landscape of lake and mountain | a Chinese landscape| a sea landscape| a city landscape}` will set the theme and automatically randomly select a theme to generate prompt.
-
--Leave more usage options to you to develop on your own.
-
--Generate effect:
-
-![](https://github.com/billwuhao/ComfyUI_OneButtonPrompt_Flux/blob/master/images/2025-02-19_20-00-01.png)
-
-**Tip**: To solve the problem of OOM, you can replace Flux with sdxl or SD15 and adjust the DeepSeek R1 prompt. Think on your own.
-
-[2025-01-21]⚒️: 
-
-- `txt` file support `//` comments: The line for comments will be ignored.
-
-- Add more prompts (human and poses):
-
-![](https://github.com/billwuhao/ComfyUI_OneButtonPrompt_Flux/blob/master/images/2025-01-25_22-47-23.png)
-![](https://github.com/billwuhao/ComfyUI_OneButtonPrompt_Flux/blob/master/images/2025-01-25_22-49-40.png)
-![](https://github.com/billwuhao/ComfyUI_OneButtonPrompt_Flux/blob/master/images/2025-01-25_22-58-00.png)
-
-[2025-01-10]⚒️: 
-
-- human_pose → pose: Any subject can set pose. Please promptly change the file name `human_pose` to `pose`.
-
-- Add 4 `add_` files to record the good prompt words I have found. You can also share your prompt words and submit an PR.
-
-- Add `None` to `subject` to make it optional, so that custom subject can be fixed in `lora_trigger_or_prefix` to generate images under the same subject and win prizes.
-
-- To add a testing mode, please put the prompts you need to test into the file `test.txt`(The first time you need to create a new one), and then turn on the testing mode to test each prompt in sequence. **Note**: During testing mode, all options except for the custom `lora_trigger_or_prefix` are invalid. f you are running the test automatically, after all prompts are completed, switch to a random generated image.
-
-  ![](https://github.com/billwuhao/ComfyUI_OneButtonPrompt_Flux/blob/master/images/2025-01-10_12-07-54.png)
-
-## Usage
-
-- **If "human" is selected**, it will generate only the human-related prompt.
-- **If "other" is selected**, it will generate only the other-related prompt.
-- **If "dual_subject" is selected**, it will include both of the above.
-- **If "pose" is enabled**, it will generate a subject pose prompt.
-- **If "style" is enabled**, it will generate a style prompt.
-- **lora_trigger_or_prefix** are optional and can be customized.
-- If you have modified the custom file during the generation images, please enable **refresh** to update it, without having to restart the software
-- **If the seed is fixed**, it will generate the same fixed prompt.
-
-![alt text](https://github.com/billwuhao/ComfyUI_OneButtonPrompt_Flux/blob/master/images/image-1.png)
-
-**Example Prompt**:  
-
-(MW:1.2), a beautiful girl, fall asleep, A beautiful lovely alpaca, naive art
-
-- `(MW:1.2)` is the **Lora trigger word**.
-- `a beautiful girl` is the **human**.
-- `fall asleep` is the **pose**.
-- `A beautiful lovely alpaca` is the **other**.
-- `naive art` is the **style**.
-
-## Custom Subjects, Poses, and Styles
-
-1. Create four files under the folder `ComfyUI\custom_nodes\ComfyUI_OneButtonPrompt_Flux\txtfiles`.
-
-   ![alt text](https://github.com/billwuhao/ComfyUI_OneButtonPrompt_Flux/blob/master/images/image.png)
-
-2. Input the corresponding content into each file to generate your custom subject, pose, and style.
-
-3. The `prompt_words.xlsx` file contains prompts I gathered a long time ago when experimenting with **Disco Diffusion** and **SD 1.5**. You can refer to it for ideas.
-
-**Note**: Be sure to create new files and modify them freely; this will not affect the node's functionality or future updates.
-
-## Automatic Image Generation
-
-If you want to automatically generate images while your computer is running, select `Queue (On Change)` and click Start. Workflow in `workflow_example`.
-
-![alt text](https://github.com/billwuhao/ComfyUI_OneButtonPrompt_Flux/blob/master/images/image-2.png)
-
-## Using with Prompt Enhancer
-
-The enhancer of prompt has a poor effect in some cases, completely destroying the style and artistic conception of the prompt. It is recommended to enable it during the lottery
-
-ComfyUI-Fluxpromptenhancer: https://github.com/marduk191/ComfyUI-Fluxpromptenhancer
-
-## Thank you
-
-Thank you to the amazing [OneButtonPrompt](https://github.com/AIrjen/OneButtonPrompt) for inspiration.
+(OneButtonPrompt)(https://github.com/AIrjen/OneButtonPrompt)
